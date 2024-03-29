6c4e2b147d67a6fc238412b0d00963adb0bd8256
Evgenii Khazanov <46842325+EvgeniyKhazanov@users.noreply.github.com>
2024-03-29 15:43:22 +0300

    add cycle code

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра 'ТИР'")
-icon = pygame.image.load("img/goggame-1240899991.ico")
-pygame.display.set_icon(icon)
+#icon = pygame.image.load("img/goggame-1240899991.ico")
+#pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/target.png")
 target_width = 80
@@ -17,11 +17,24 @@ target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0, 255), (random.randint(0, 255), (random.randint(0, 255))
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img,(target_x, target_y))
+
+    pygame.display.update()
+
 
 pygame.quit()
