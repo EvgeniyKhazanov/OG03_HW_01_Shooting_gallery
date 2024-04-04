91580a6eb957bcd91a8d22c795aee808f254ec2e
Evgenii Khazanov <46842325+EvgeniyKhazanov@users.noreply.github.com>
2024-04-04 08:58:27 +0300

    Добавлен подсчет очков

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -19,8 +20,23 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+target_radius = 50
+target_position = (random.randint(target_radius, SCREEN_WIDTH - target_radius),
+                   random.randint(target_radius, SCREEN_HEIGHT - target_radius))
+
+
+def calculate_score(click_position, target_position):
+    distance = math.sqrt((click_position[0] - target_position[0]) ** 2 + (click_position[1] - target_position[1]) ** 2)
+    score = max(0, round((target_radius - distance) * 2))  # Примерный расчет очков
+    return score
 
 running = True
+score = 0
+
+# Определение координат центра мишени
+target_center_x = target_x + target_width / 2
+target_center_y = target_y + target_height / 2
+
 while running:
     screen.fill(color)
     for event in pygame.event.get():
@@ -28,13 +44,21 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+            # Проверка, попал ли клик в мишень
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                # Вызываем функцию calculate_score для обновления счета
+                score += calculate_score((mouse_x, mouse_y), (target_center_x, target_center_y))
+                # Генерируем новое положение мишени
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                # Обновляем координаты центра мишени после ее перемещения
+                target_center_x = target_x + target_width / 2
+                target_center_y = target_y + target_height / 2
+            
+            print(f"Текущий счет: {score}")
 
-    screen.blit(target_img,(target_x, target_y))
+    screen.blit(target_img, (target_x, target_y))
 
     pygame.display.update()
-
 
 pygame.quit()
